2370c606239cd6cbaa3dc3c6e3da10bcd1d81469
Lucas Sacramento <sacramento.15@hotmail.com>
2025-11-20 19:33:05 -0300

    feat: create analyzer

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,17 @@ def calculate_sum(a, b):
     result = a + b
     return result
 """
+
+@pytest.fixture
+def sample_commits_list(sample_commit_data):
+    """Lista de commits de exemplo."""
+    commits = [sample_commit_data.copy()]
+    
+    # Adiciona mais um commit
+    commit2 = sample_commit_data.copy()
+    commit2['hash'] = 'xyz789ghi012'
+    commit2['author'] = 'Another Author'
+    commit2['date'] = datetime(2024, 1, 2, 12, 0, 0)
+    commits.append(commit2)
+    
+    return commits
